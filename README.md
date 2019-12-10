36bbbeda5a2b468cc75c452c2db33a834d373095
schuchardt1 <schuchardt1@softed.de>
2019-12-10 13:51:48 +0100

    various updates

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CitrixPrinterAutomation
 Provides cmdlets to automate Printer Assignment Policy Creation
+
+# Prerequisites
+* Install Citrix Group Policy Management
+* Load Citrix.GroupPolicy.Commands from literally anywhere you're able to find it
--- function-New-PrinterAssignmentPolicy.ps1
+++ function-New-PrinterAssignmentPolicy.ps1
@@ -19,68 +19,30 @@ function New-PrinterAssignmentPolicy {
         # Mode for handling policy conflicts
         [Parameter(Mandatory=$false)]
         [ValidateSet("Overwrite","Append","Create")]
-        [String]$Mode
+        [String]$Mode,
+
+        # File Path to Citrix GPO Module psm1
+        [Parameter(Mandatory)]
+        [ValidateScript({Test-Path $_})]
+        [String]
+        $CtxGpoModulePath
     )
 
     begin {
-        function Test-CtxPolicy {
-            param (
-                [String]$PolicyName,
-                [String]$Scope = "Computer",
-                [System.Management.Automation.PSDriveInfo]$PSDriveObject
-            )
-            $targetPath = ("Citrix.GroupPolicy.Commands\$($PSDriveObject.Provider.Name)" + "::$($PSDriveObject.Name)" + ":\$Scope" + "\$PolicyName")
-            Write-Verbose "Checking for existing Policy: $targetPath"
-            if (Get-Item $targetPath -ErrorAction SilentlyContinue) {
-                return $true
-            }
-            else {
-                return $false
-            }
-        }
-
-        function Get-CtxPolicy {
-            param (
-                [String]$PolicyName,
-                [String]$Scope = "Computer",
-                [System.Management.Automation.PSDriveInfo]$PSDriveObject
-            )
-            $targetPath = ("Citrix.GroupPolicy.Commands\$($PSDriveObject.Provider.Name)" + "::$($PSDriveObject.Name)" + ":\$Scope" + "\$PolicyName")
-            Write-Verbose "Checking for existing Policy: $targetPath"
-            $obj = get-item $targetPath -ErrorAction SilentlyContinue
-            return $obj
-        }
-
-        function Remove-CtxPolicy {
-            param (
-                [String]$PolicyName,
-                [String]$Scope = "Computer",
-                [System.Management.Automation.PSDriveInfo]$PSDriveObject
-            )
-            $targetPath = ("Citrix.GroupPolicy.Commands\$($PSDriveObject.Provider.Name)" + "::$($PSDriveObject.Name)" + ":\$Scope" + "\$PolicyName")
-            Write-Verbose "Removing existing Policy: $targetPath"
-            try {
-                (get-item $targetPath).Remove()
-            }
-            catch {
-                return $false
-            }
-
-        }
-
         Add-PSSnapin Citrix*
         if (-not (Get-PSSnapin Citrix.Common.GroupPolicy -ErrorAction SilentlyContinue))
         {
-            Add-PSSnapin Citrix.Common.GroupPolicy -ErrorAction Stop
+            Add-PSSnapin Citrix.Common.GroupPolicy -ErrorAction Stop
         }
-        #Import-Module Citrix.GroupPolicy.Commands
+        Import-Module $CtxGpoModulePath
+        #Import-Module "C:\Program Files\Citrix\PowerShellModules\Citrix.GroupPolicy.Commands\Citrix.GroupPolicy.Commands.psd1" -Force
     }
 
     process {
         # Loading Environment
         Write-Verbose "Creating Citrix Policy Drive"
         try {
-            $ctxDrive = New-PSDrive -Name LocalFarmGpo -PSProvider Citrix.GroupPolicy.Commands\CitrixGroupPolicy -Controller $AdminAddress -Root "\" -ErrorAction Stop
+            $ctxDrive = New-PSDrive -Name LocalFarmGpo -PSProvider Citrix.Common.GroupPolicy\CitrixGroupPolicy -Controller $AdminAddress -Root "\" -ErrorAction Stop -Scope Global
         }
         catch [System.Management.Automation.ActionPreferenceStopException] {
             Write-Error "Could not create drive: $($error[0].Exception.Message)"
@@ -89,35 +51,67 @@ function New-PrinterAssignmentPolicy {
 
         # Do stuff
         Write-Verbose "Checking for existing policy: $PolicyName"
-        $policyExists = Test-CtxPolicy -PolicyName $PolicyName -Scope "User" -PSDriveObject $ctxDrive
-        if ($policyExists) {
-            switch ($Mode) {
-                "Overwrite" {
-                    Write-Verbose "Existing policy will be overwritten"
-                    Remove-CtxPolicy -PolicyName $PolicyName -Scope User -PSDriveObject $ctxDrive
-                }
-                "Merge" {
-                    Write-Error "Not yet implemented"
-                    return $null
-                }
-                "Create" {
-                    Write-Warning "Will not re-create existing policy"
-                    return $null
+        $ctxGpo = Get-CtxGroupPolicy -PolicyName $PolicyName -Type "User" -DriveName $ctxDrive.Name
+        try {
+            if ($ctxGpo) {
+                switch ($Mode) {
+                    "Overwrite" {
+                        Write-Verbose "Existing policy will be overwritten"
+                        Remove-CtxGroupPolicy -PolicyName $PolicyName -Type User -DriveName $ctxDrive.Name
+                        Write-Verbose "Creating Policy: $PolicyName"
+                        $ctxGpo = New-CtxGroupPolicy -PolicyName $PolicyName -Type User -DriveName $ctxDrive.Name -ErrorAction Stop
+                    }
+                    "Create" {
+                        Write-Warning "Will not re-create existing policy"
+                        Get-PSDrive -PSProvider Citrix.Common.GroupPolicy\CitrixGroupPolicy | Remove-PSDrive
+                        return $null
+                    }
                 }
             }
+            else {
+                Write-Verbose "Creating Policy: $PolicyName"
+                $ctxGpo = New-CtxGroupPolicy -PolicyName $PolicyName -Type User -ErrorAction Stop
+            }
         }
+        catch [System.management.Automation.ActionPreferenceStopException] {
+            Write-Error "Could not create policy object or Printer Assignment Objects: $($error[0].Exception.Message)"
+            Get-PSDrive -PSProvider Citrix.Common.GroupPolicy\CitrixGroupPolicy | Remove-PSDrive
+            return $false
+        }
+        
+        $obj = ($ctxGpo | Get-CtxGroupPolicyConfiguration).PrinterAssignments.Assignments
+        Get-PSDrive -PSProvider Citrix.Common.GroupPolicy\CitrixGroupPolicy | Remove-PSDrive
+        return $obj
+
+        <#
         $newPolicyPath = Join-Path -Path "$($ctxDrive):\User" -ChildPath $PolicyName
         try {
             New-Item $newPolicyPath -ErrorAction Stop
+            $ctxGpo = Get-CtxGroupPolicy -PolicyName $PolicyName -Type "User" -DriveName $ctxDrive.Name -ErrorAction SilentlyContinue
+            <#
+            $printerData = get-content $InputFile | ConvertFrom-Json
+            foreach ($printerRule in $printerData) {
+                Write-Verbose "Adding policy for printer $($printerRule.PrinterName)"
+                $currentAssignments = ($ctxGpo | Get-CtxGroupPolicyConfiguration).PrinterAssignments
+                $currentAssignments
+            }
+            
         }
         catch [System.Management.Automation.ActionPreferenceStopException]{
-            Write-Error "Could not create policy object: $($error[0].Exception.Message)"
+            Write-Error "Could not create policy object or Printer Assignment Objects: $($error[0].Exception.Message)"
             return $false
         }
-        # Cleanup Environment
-        Write-Verbose "Removing PS Drive"
-        Remove-PSDrive LocalFarmGpo
+        #>
     }
 }
 
-New-PrinterAssignmentPolicy -AdminAddress vcitrix201.softed.de -InputFile .\TestData\printer.json -PolicyName "DruckerTest" -Verbose -Mode Overwrite
+$newPolHash = @{
+    #AdminAddress = "vcitrix201"
+    InputFile = "C:\Code\CitrixPrinterAutomation\TestData\printer.json"
+    PolicyName = "DruckerTest2"
+    Verbose = $true
+    Mode = "Append"
+    CtxGpoModulePath = "C:\Program Files\Citrix\Telemetry Service\TelemetryModule\Citrix.GroupPolicy.Commands.psm1"
+}
+
+New-PrinterAssignmentPolicy @newPolHash
